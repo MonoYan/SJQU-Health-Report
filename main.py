8aad4ffbb0bb5fa1b4c547fc6d9a004b9cab1e4f
Wang Yan <43719212+MonoYan@users.noreply.github.com>
2022-11-04 23:28:54 +0800

    Update main.py
    Not in school

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 from GenchAPI import GenchAPI
-GenchAPI.sign(os.environ.get('username') , os.environ.get('password'), 0)
+GenchAPI.sign(os.environ.get('username') , os.environ.get('password'))
