ee44cf4fc0829d231559b7be1f6c10f615513ecf
Wang Yan <43719212+MonoYan@users.noreply.github.com>
2022-12-13 22:32:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 from GenchAPI import GenchAPI
-GenchAPI.sign(os.environ.get('username') , os.environ.get('password'), 0, "上海市", "上海市", "虹口区")
+GenchAPI.sign(os.environ.get('username') , os.environ.get('password'), 0, "上海", "上海市", "虹口区")
